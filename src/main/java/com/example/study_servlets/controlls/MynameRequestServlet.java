d7751b2bcffd297f260ac9d8688b8bcda03ba18f
lee000403 <dong000403@gmail.com>
2023-06-20 15:55:16 +0900

    commiot

--- src/main/java/com/example/study_servlets/controlls/MynameRequestServlet.java
+++ src/main/java/com/example/study_servlets/controlls/MynameRequestServlet.java
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/mynameREquestServlet")
-public class MynameREquestServlet extends HttpServlet{
+@WebServlet(urlPatterns = "/mynameRequestServlet")
+public class MynameRequestServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
