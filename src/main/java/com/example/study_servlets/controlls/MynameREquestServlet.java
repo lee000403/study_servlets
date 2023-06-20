6e95a7e4de2f024f513a647f46f3c1c1f68717cc
lee000403 <dong000403@gmail.com>
2023-06-20 15:52:02 +0900

    commiititmtmtmitmititmit

--- src/main/java/com/example/study_servlets/controlls/MynameREquestServlet.java
+++ src/main/java/com/example/study_servlets/controlls/MynameREquestServlet.java
@@ -9,34 +9,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/getRequestParamsServlet")
-public class GetRequestParamsServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/mynameREquestServlet")
+public class MynameREquestServlet extends HttpServlet{
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            // PrintWriter printWriter = response.getWriter();
-            // String contents = "";
-            // printWriter.println(contents);
-            // printWriter.close();
-            String firstName = request.getParameter("first_name");
-            String secondName = request.getParameter("second_name");
             PrintWriter printWriter = response.getWriter();// response.getWriter();네크워크에 응답하고 작성할꺼야 실어보내는거야
-            // String contents = "GABAE OH !";
             String contents = "<!DOCTYPE html>\r\n" + //
                     "<html lang=\"en\">\r\n" + //
                     "<head>\r\n" + //
                     "    <meta charset=\"UTF-8\">\r\n" + //
                     "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
-                    "    <title>Hello World !</title>\r\n" + //
+                    "    <title>Document</title>\r\n" + //
                     "</head>\r\n" + //
                     "<body>\r\n" + //
-                    "    <div> first Name : " + firstName + " </div>\r\n" + //
-                    "    <div> second Name : " + secondName + " </div>\r\n" + //
+                    "    <div>input your name</div>\r\n" + //
+                    "    <form action=\"/getRequestParamsServlet\" method=\"get\">\r\n" + //
+                    "        <div>\r\n" + //
+                    "            first name : <input type=\"text\" name = \"first_name\">\r\n" + //
+                    "        </div>\r\n" + //
+                    "        <div>\r\n" + //
+                    "            second name : <input type=\"text\" name = \"second_name\">\r\n" + //
+                    "        </div>\r\n" + //
+                    "        <div>\r\n" + //
+                    "            <button type=\"submit\">\uBCF4\uB0B4\uAE30</button>\r\n" + //
+                    "        </div>\r\n" + //
+                    "    </form>\r\n" + //
                     "</body>\r\n" + //
                     "</html>";
             printWriter.println(contents);
-            printWriter.close(); // 네크워크에 실어보냈고 CLOSED
+            printWriter.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
