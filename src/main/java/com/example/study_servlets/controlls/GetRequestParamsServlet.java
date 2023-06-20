a3076761801e308f5b3d90ddf4f3244e3c1df9ad
lee000403 <dong000403@gmail.com>
2023-06-20 12:24:08 +0900

    나는 된다.

---
+++ src/main/java/com/example/study_servlets/controlls/GetRequestParamsServlet.java
@@ -0,0 +1,44 @@
+package com.example.study_servlets.controlls;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/GetRequestParamsServlet")
+public class GetRequestParamsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            // PrintWriter printWriter = response.getWriter();
+            // String contents = "";
+            // printWriter.println(contents);
+            // printWriter.close();
+            String firstName = request.getParameter("first_name");
+            String secondName = request.getParameter("second_name");
+            PrintWriter printWriter = response.getWriter();// response.getWriter();네크워크에 응답하고 작성할꺼야 실어보내는거야
+            // String contents = "GABAE OH !";
+            String contents = "<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title>Hello World !</title>\r\n" + //
+                    "</head>\r\n" + //
+                    "<body>\r\n" + //
+                    "    <div> first Name : " + firstName + " </div>\r\n" + //
+                    "    <div> second Name : " + secondName + " </div>\r\n" + //
+                    "</body>\r\n" + //
+                    "</html>";
+            printWriter.println(contents);
+            printWriter.close(); // 네크워크에 실어보냈고 CLOSED
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
