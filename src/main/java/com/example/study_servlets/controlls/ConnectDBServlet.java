f6478e9bfb746fd21baba7ccc2730f5bd328d78c
lee000403 <dong000403@gmail.com>
2023-06-21 17:00:40 +0900

    commit

--- src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
+++ src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.example.commons.Common;
 import com.example.study_servlets.Commons;
 
 @WebServlet(urlPatterns = "/ConnectDBServlet")
