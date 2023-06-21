c55834081222a4aaaf73f7fde17fc3636b4d3f52
lee000403 <dong000403@gmail.com>
2023-06-21 16:41:45 +0900

    COMIT

--- src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
+++ src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
@@ -35,7 +35,27 @@ public class ConnectDBServlet extends HttpServlet {
             System.out.println("DB연결 성공\n");
 
             // 클라이언트에 html 화면 제공
-            String contents = "yoju lab";
+            String contents = "<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title>Bootstrap Template</title>\r\n" + //
+                    "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.css\">\r\n" + //
+                    "    <link rel=\"stylesheet\" href=\"../css/commons.css\">\r\n" + //
+                    "</head>\r\n" + //
+                    "\r\n" + //
+                    "<body>\r\n" + //
+                    "    <table class=\"table table-bordered table-hover\">\r\n" + //
+                    "        <thead>\r\n" + //
+                    "            <tr>\r\n" + //
+                    "                <th>COMPANY_ID</th>\r\n" + //
+                    "                <th>COMPANY</th>\r\n" + //
+                    "            </tr>\r\n" + //
+                    "        </thead>\r\n" + //
+                    "        <tbody>\r\n";
 
             // - query Edit
             Statement statement = connection.createStatement();
@@ -46,9 +66,15 @@ public class ConnectDBServlet extends HttpServlet {
             while (resultSet.next()) { // next: 뭉치를 던져줌
                 // resultSet.getString("COMPANY_ID");
                 // resultSet.getString("COMPANY");
-                System.out.println(resultSet.getString("COMPANY_ID") + "" + resultSet.getString("COMPANY"));
+                contents = contents + 
+                    "            <tr>\r\n" + //
+                    "                <td>"+resultSet.getString("COMPANY_ID")+"</td>\r\n" + //
+                    "                <td>"+resultSet.getString("COMPANY")+"</td>\r\n" + //
+                    "            </tr>\r\n";
             }
-            
+
+            response.setContentType("text/html;charset=UTF-8");
+
             // 클라이언트에 html 화면 제공
             PrintWriter printWriter = response.getWriter();
             printWriter.println(contents);
@@ -62,6 +88,13 @@ public class ConnectDBServlet extends HttpServlet {
                 System.out.println(resultSet.getInt("CNT"));
                 totalCount = resultSet.getInt("CNT");
             }
+            contents = contents +                     "        </tbody>\r\n" + //
+                    "    </table>\r\n" + //
+                    "</body>\r\n" + //
+                    "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.js\"></script>\r\n" + //
+                    "\r\n" + //
+                    "</html>";
+
 
             /*
              * - 값이 테이블로 나오지 않음
